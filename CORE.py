dee69ef742b2478f1a7585c0c6e51657ce156397
Manoj Kumar .T <manojkumartalamanchi7@gmail.com>
2026-06-24 14:38:35 +0530

    Updated tracker code with recent changes

--- CORE.py
+++ CORE.py
@@ -1,6 +1,6 @@
-print("--- Gitam Student Expense Tracker Started ---")
+print("--- Student Expense Tracker Started ---")
 
-budget = 5000
+budget = int(input("Enter your monthly budget limit (in Rs.): "))
 total_spent = 0
 
 print(f"Your monthly budget limit is: Rs. {budget}")
@@ -9,18 +9,18 @@ while True:
     print("\n-------------------------------")
     user_input = input("Enter expense amount (or type 'exit' to stop): ")
     
-    # Check if the user wants to quit
+    #  if the user wants to quit
     if user_input.lower() == 'exit':
         print("Saving your report and closing tracker...")
         with open("expense_report.txt", "w") as file:
-            file.write("--- GITAM EXPENSE REPORT ---\n")
+            file.write("--- STUDENT EXPENSE REPORT ---\n")
             file.write(f"Initial Budget: Rs. {budget}\n")
             file.write(f"Total Amount Spent: Rs. {total_spent}\n")
             file.write(f"Final Remaining Balance: Rs. {budget - total_spent}\n")
         print("✅ 'expense_report.txt' saved successfully!")
         break
         
-    # --- THIS IS THE MISSING MATH BLOCK ---
+    # --- MATH BLOCK ---
     expense = int(user_input)
     total_spent = total_spent + expense
     remaining = budget - total_spent
